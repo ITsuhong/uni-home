3f1aa8db052fd53b29997d767135d9265310323e
suhong <1844966932@qq.com>
2024-11-06 22:10:29 +0800

    update

---
+++ pages/albumDetail/index.vue
@@ -0,0 +1,58 @@
+<script setup>
+	import {
+		ref
+	} from 'vue'
+
+	const images = ref([
+		'https://fakeimg.pl/350x400/?text=World&font=lobster',
+		'https://fakeimg.pl/350x250/?text=World&font=lobster',
+		'https://fakeimg.pl/350x300/?text=World&font=lobster',
+		'https://fakeimg.pl/350x450/?text=World&font=lobster',
+		'https://fakeimg.pl/350x280/?text=World&font=lobster',
+		'https://fakeimg.pl/350x320/?text=World&font=lobster',
+		'https://fakeimg.pl/350x380/?text=World&font=lobster',
+		'https://fakeimg.pl/350x420/?text=World&font=lobster',
+		'https://fakeimg.pl/350x360/?text=World&font=lobster'
+	])
+</script>
+
+<template>
+	<view class="page-container">
+		<view class="waterfall-container">
+			<view class="waterfall-item" v-for="(img, index) in images" :key="index">
+				<image :src="img" :alt="`图片${index + 1}`" mode="widthFix" />
+			</view>
+		</view>
+	</view>
+	<uni-fab :popMenu="false" horizontal="right"></uni-fab>
+</template>
+
+<style scoped>
+	.page-container {
+		width: 100%;
+		min-height: 100vh;
+		background: #f5f5f5;
+	}
+
+	.waterfall-container {
+		column-count: 2;
+		column-gap: 10px;
+		padding: 10px;
+		width: 100%;
+		box-sizing: border-box;
+	}
+
+	.waterfall-item {
+		break-inside: avoid;
+		margin-bottom: 10px;
+		background: #fff;
+		border-radius: 8px;
+		overflow: hidden;
+	}
+
+	.waterfall-item image {
+		width: 100%;
+		height: auto;
+		display: block;
+	}
+</style>
--- pages/albumSet/index.vue
+++ pages/albumSet/index.vue
@@ -1,19 +1,31 @@
 <script setup>
-
+import {ref} from "vue"
+const popup=ref()
+const hanleOptionCreate=()=>{
+	popup.value?.open()
+}
+const name=ref('');
+const handleOptionDetail=()=>{
+	uni.navigateTo({
+		url:"/pages/albumDetail/index"
+	})
+}
 </script>
 
 <template>
 	<view class="p-3">
 		<view class="grid grid-cols-2 gap-4">
-			<view class="w-42 bg-white rounded-lg pb-1">
-				<image class="w-full h-32 rounded-t-lg" mode="aspectFill" src="https://fakeimg.pl/350x200/?text=World&font=lobster"></image>
+			<view @click="handleOptionDetail" class="w-42 bg-white rounded-lg pb-1">
+				<image class="w-full h-32 rounded-t-lg" mode="aspectFill"
+					src="https://fakeimg.pl/350x200/?text=World&font=lobster"></image>
 				<view class="flex justify-between px-2">
 					<view>电影票</view>
 					<view>共 0 张</view>
 				</view>
 			</view>
 			<view class="w-42  bg-white rounded-lg pb-1">
-				<image class="w-full h-32 rounded-t-lg" mode="aspectFill" src="https://fakeimg.pl/350x200/?text=World&font=lobster"></image>
+				<image class="w-full h-32 rounded-t-lg" mode="aspectFill"
+					src="https://fakeimg.pl/350x200/?text=World&font=lobster"></image>
 				<view class="flex justify-between px-2 text-sm">
 					<view>电影票</view>
 					<view>共 0 张</view>
@@ -21,6 +33,23 @@
 			</view>
 		</view>
 	</view>
+	<view class="fixed flex justify-center left-0 right-0 bottom-20">
+		<view @click="hanleOptionCreate" class="text-[#999999] px-12 py-2 bg-white rounded-full flex items-center">
+			<uni-icons color="#999999" type="plusempty" size="26"></uni-icons>
+			<view class="ml-2">创建新的相册集</view>
+		</view>
+	</view>
+	<uni-popup ref="popup" type="center" border-radius="10px 10px 0 0">
+		<view class="w-[600rpx] h-32 bg-white rounded-md p-4">
+			<view class="bg-[#F5F7FA] p-3 rounded-lg">
+				<input  placeholder="请输入相册名称" v-model="name"/>
+			</view>
+			<view class="flex justify-end mt-3">
+				<view class="text-white px-5 py-2 bg-[#4096ff] rounded-lg">确定</view>
+			</view>
+			
+		</view>
+	</uni-popup>
 </template>
 
 
